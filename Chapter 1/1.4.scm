78cc83bdb4abd70d1cb51e7b0b524340a05421dc
A4RO <102383141+A4RO@users.noreply.github.com>
2024-11-21 11:45:26 +0200

    Update 1.4.scm

--- Chapter 1/1.4.scm
+++ Chapter 1/1.4.scm
@@ -1,6 +1,1 @@
-;Describe the behavior of the following procedure:
-
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-
 ;The procedure calculates the sum of a and absolute value of b. If b is positive, a+b is calculated. If b is negative, a-b is calculated and since b is negative it's the same as a+|b|.
